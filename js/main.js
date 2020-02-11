98cb0a85f710f0690c119b95e7c4cb008a6d6768
Juan C. Romualdo <jromualdo@razimports.com>
2020-02-11 17:02:41 -0600

    droppin js file here for now

---
+++ js/main.js
@@ -0,0 +1,89 @@
+//DOM Elements
+const time = document.getElementById('time'),
+    greeting = document.getElementById('greeting'),
+    name = document.getElementById('name'),
+    focus = document.getElementById('focus');
+
+//Show Time
+function showTime() {
+    let today = new Date(),
+        hour = today.getHours(),
+        min = today.getMinutes(),
+        sec = today.getSeconds();
+
+    // Set AM or PM
+    const amPm = hour >= 12 ? 'PM' : 'AM';
+
+    // 12hr Format
+    hour = hour % 12 || 12;
+
+    // Output Time
+    time.innerHTML = `${hour}<span>:</span>${addZeros(min)}<span>:</span>${addZeros(sec)}`;
+
+    setTimeout(showTime, 1000);
+}
+
+// Add Zeros for 0 - 9 seconds
+function addZeros(n) {
+    return (parseInt(n, 10) < 10 ? '0' : '') + n;
+}
+
+//Set Background and Greeting
+function setBgGreet() {
+    let today = new Date(),
+    hour = today.getHours();
+
+    if(hour < 12) {
+        // Morning
+        document.body.style.backgroundImage = "url('../img/morning.jpg')";
+        greeting.textContent = 'Good Morning';
+    } else if(hour < 18) {
+        //Afternoon
+        document.body.style.backgroundImage = "url('../img/afternoon.jpg')";
+        greeting.textContent = 'Good Afternoon';
+    } else {
+        //Evening
+        document.body.style.backgroundImage = "url('../img/evening.jpg')";
+        greeting.textContent = 'Good Evening';
+    }
+}
+
+// Get Name
+function getName() {
+    if(localStorage.getItem('name') === null) {
+        name.textContent = '[Enter Name]'
+    } else {
+        name.textContent = localStorage.getItem('name')
+    }
+}
+
+// Set Name
+function setName(e) {
+    if(e.type === 'keypress') {
+        // Make sure enter is pressed
+        if(e.which == 13 || e.keyCode == 13) {
+            localStorage.setItem('name', e.target.innerText);
+            name.blur();
+        }
+    } else {
+        localStorage.setItem('name', e.target.innerText);
+    }
+}
+
+// Get Focus
+function getFocus() {
+    if(localStorage.getItem('focus') === null) {
+        focus.textContent = '[Enter Focus]'
+    } else {
+        focus.textContent = localStorage.getItem('focus')
+    }
+}
+
+name.addEventListener('keypress', setName);
+name.addEventListener('blur', setName)
+
+// Run
+showTime();
+setBgGreet();
+getName();
+getFocus();
